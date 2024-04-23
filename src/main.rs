4fea8f432e29bda971296736119f9207a6da5a48
nikekukuri <nikekukuri0327@gmail.com>
2024-04-23 16:02:18 +0900

    Implement test

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,15 @@ async fn main() {
         .unwrap();
 }
 
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+}
+
 /// GET http://localhost:3000
 async fn root() -> &'static str {
-    "Hello, world!"
+    "Hello, World!"
 }
 
 /// POST http://localhost:3000/users
@@ -43,13 +49,54 @@ async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     (StatusCode::CREATED, Json(user))
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 struct CreateUser {
     username: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 struct User {
     id: u64,
     username: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{header, Method, Request},
+    };
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .uri("/users")
+            .method(Method::POST)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(r#"{ "username": "guy" }"#))
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        let user: User = serde_json::from_str(&body).expect("cannot conver User instance.");
+        assert_eq!(user, User{
+            id: 1337,
+            username: "guy".to_string(),
+        });
+
+    }
+
 }
