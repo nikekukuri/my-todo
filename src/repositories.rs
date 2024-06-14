f56233950fe7798399cbcaf98824f16f8db708fb
nikekukuri <nikekukuri0327@gmail.com>
2024-06-14 11:17:09 +0900

    Add Docker environment, Makefile and sqlx crates.

--- src/repositories.rs
+++ src/repositories.rs
@@ -5,6 +5,7 @@ use std::{
 };
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use validator::Validate;
 
 #[derive(Debug, Error)]
 enum RepositoryError {
@@ -27,8 +28,10 @@ pub struct Todo {
     completed: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Validate)]
 pub struct CreateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: String,
 }
 
@@ -40,8 +43,10 @@ impl CreateTodo {
 }
 
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Validate)]
 pub struct UpdateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: Option<String>,
     completed: Option<bool>,
 }
